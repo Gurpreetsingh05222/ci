12c5f1a1ed9af2ece7be96e9ab18b52e70d58666
Gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2019-06-28 23:36:35 +0530

    ci files

---
+++ application/views/welcome_message.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Sample App</title>
+</head>
+<body>
+	<h1>This is codeigniter Welcome page.</h1>
+</body>
+</html>
