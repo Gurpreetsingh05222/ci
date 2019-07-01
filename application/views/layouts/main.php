6643cfc17009942fb58d0136a596c4e74537ab36
Gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2019-07-01 22:15:39 +0530

    main.php view and main content

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -3,11 +3,16 @@
 <head>
 	<meta charset="utf-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="<?php base_url();?>assets/css/bootstrap.min.css">
-	<script src="<?php base_url();?>assets/js/bootstrap.min.js"></script>
-	<script src="<?php base_url();?>assets/js/jquery.min.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+	<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
 </head>
 <body>
-
+	<div class="container">
+		<div class="col-xs-3"></div>
+		<div class="col-xs-9">
+			<?php $this->load->view($main_view); ?>	
+		</div>
+	</div>
 </body>
 </html>
