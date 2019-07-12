bcf00dc554e1476fbc3ea201d5437359f5783494
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2019-07-12 10:48:45 +0530

    displaying projects

--- application/views/projects/index.php
+++ application/views/projects/index.php
@@ -1,1 +1,22 @@
-<h1 class="text-center">Projects</h1>
+<h1>Projects</h1>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>
+                Project Name
+            </th>
+            <th>
+                Project Body
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($projects as $project):?>
+            <tr>
+                <?php echo "<td>".$project->project_name."</td>" ; ?>
+                <?php echo "<td>".$project->project_body."</td>" ; ?>
+            </tr>
+        <?php endforeach;?>
+    </tbody>
+</table>
